fa9df7db5cea84456faa60dcf62762899b4c61be
shriya <shriyachandel1998@gmail.com>
2024-03-28 21:08:47 +0530

    update index

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -17,4 +17,5 @@ return array(
     'Google\\Service\\' => array($vendorDir . '/google/apiclient-services/src'),
     'Google\\Auth\\' => array($vendorDir . '/google/auth/src'),
     'Firebase\\JWT\\' => array($vendorDir . '/firebase/php-jwt/src'),
+    'Facebook\\' => array($vendorDir . '/facebook/graph-sdk/src/Facebook'),
 );
