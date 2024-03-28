3700f2b8e79b52fd15b300c7a4bde99edfee98be
shriya <shriyachandel1998@gmail.com>
2024-03-28 21:28:47 +0530

    update index

--- index.php
+++ index.php
@@ -22,6 +22,27 @@ if (isset($_GET['code'])) {
 $app_id = 'YOUR_APP_ID';
 $app_secret = 'YOUR_APP_SECRET';
 
+   session_start();
+
+   $fb = new Facebook\Facebook([
+      'app_id' => $app_id,
+      'app_secret' => $app_secret,
+      'default_graph_version' => 'v13.0',
+   ]);
+
+   $helper = $fb->getRedirectLoginHelper();
+   $accessToken = $helper->getAccessToken();
+
+   if ($accessToken) {
+      // User is logged in, handle their data
+      $user = $fb->get('/me', ['fields' => 'id,name,email']);
+      $_SESSION['user_data'] = $user;
+      header('Location: profile.php');
+   } else {
+      // User is not logged in, redirect to login page
+      $loginUrl = $helper->getLoginUrl(['scope' => 'public_profile,email']);
+      header('Location: ' . $loginUrl);
+   }
 ?>
 <script src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v13.0&appId=YOUR_APP_ID&autoLogApp=true" async defer></script>
 <button id="facebook-login-button">Login with Facebook</button>
